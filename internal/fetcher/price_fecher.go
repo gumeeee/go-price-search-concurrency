5dc1973576652cea5449d9b7a2095ad614947e9f
gumeeee <gaxco123@gmail.com>
2025-05-11 03:29:44 -0300

    feat: Add additional goroutine for fetching multiple prices in FetchPrices function

--- internal/fetcher/price_fecher.go
+++ internal/fetcher/price_fecher.go
@@ -8,7 +8,7 @@ import (
 
 func FetchPrices(priceChannel chan<- float64) {
 	var wg sync.WaitGroup
-	wg.Add(3)
+	wg.Add(4)
 
 	go func() {
 		defer wg.Done()
@@ -23,6 +23,11 @@ func FetchPrices(priceChannel chan<- float64) {
 	go func() {
 		defer wg.Done()
 		priceChannel <- fetchPriceFromThirdSite()
+	}()
+
+	go func() {
+		defer wg.Done()
+		fetchAndSendMultiplePrices(priceChannel)
 	}()
 
 	wg.Wait()
@@ -45,4 +50,17 @@ func fetchPriceFromThirdSite() float64 {
 	time.Sleep(2 * time.Second)
 
 	return rand.Float64() * 100
+}
+
+func fetchAndSendMultiplePrices(priceChannel chan<- float64) {
+	time.Sleep(6 * time.Second)
+	prices := []float64{
+		rand.Float64() * 100,
+		rand.Float64() * 100,
+		rand.Float64() * 100,
+	}
+
+	for _, price := range prices {
+		priceChannel <- price
+	}
 }
